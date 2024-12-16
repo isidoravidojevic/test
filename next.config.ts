2d3be65ddadf0a2c0de69c228d7a6c3f82795296
Isidora Vidojevic <isidora.vid@gmail.com>
2024-12-16 14:17:21 +0100

    laalla

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: isProd ? '/test/' : '',
-  basePath: isProd ? "/test" : "",
-  output: "export",
+  // reactStrictMode: true,
+  // images: {
+  //   unoptimized: true,
+  // },
+  // assetPrefix: isProd ? '/test/' : '',
+  // basePath: isProd ? "/test" : "",
+  // output: "export",
 };
 
 export default nextConfig;
